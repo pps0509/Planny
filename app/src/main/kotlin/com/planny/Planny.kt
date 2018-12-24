535d5e3362438953f26ef2992f7184376d95d836
ghfid <ghfid86@gmail.com>
2018-12-24 23:17:18 +1300

    Ran on my own android device for the first time. Planny ran. It's time to test some debugging mode now.

--- app/src/main/kotlin/com/planny/Planny.kt
+++ app/src/main/kotlin/com/planny/Planny.kt
@@ -2,11 +2,13 @@ package com.planny
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 
 
 class Planny : Application () {
 
     companion object {
+        val tag = "Planny"
         var ctx: Context? = null
     }
 
@@ -14,5 +16,18 @@ class Planny : Application () {
 
         super.onCreate()
         ctx = applicationContext
+        Log.v(tag, "[ ON CREATE ]")
     }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        Log.w(tag, "[ ON LOW MEMORY ]")
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        Log.d(tag, "[ ON TRIM MEMORY ]: $level")
+    }
+
+
 }
