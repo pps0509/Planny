da47b8cf221404be5533a2cb6399a7c7077c2510
ghfid <ghfid86@gmail.com>
2018-12-27 16:16:59 +1300

    Setting up fragment classes. Had a problem with "?" while overriding onCreateView class and putting ? after LayoutInflater(? like this) caused override function not working. Also deleted ? from inflater.inflater as it was unnecessary null safe.

--- app/src/main/kotlin/com/planny/activity/MainActivity.kt
+++ app/src/main/kotlin/com/planny/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.planny.activity
 
+import android.os.Bundle
 import com.planny.R
+import com.planny.fragment.ItemsFragment
 
 class MainActivity : BaseActivity() {
 
@@ -8,6 +10,15 @@ class MainActivity : BaseActivity() {
     override val tag = "Main activity"
     override fun getLayout() = R.layout.activity_main
     override fun getActivityTitle() = R.string.app_name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fragment = ItemsFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
+    }
 
 }
 
---
+++ app/src/main/kotlin/com/planny/fragment/ItemsFragment.kt
@@ -0,0 +1,11 @@
+package com.planny.fragment
+
+import com.planny.R
+
+class ItemsFragment : BaseFragment () {
+
+    override val logTag = "Items fragment"
+    override fun getLayout() : Int {
+        return R.layout.fragment_items
+    }
+}
