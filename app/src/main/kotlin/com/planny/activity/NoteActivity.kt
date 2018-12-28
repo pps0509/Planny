85f95dd01e8816a753e064a71d56f7c3f7c62a72
ghfid <ghfid86@gmail.com>
2018-12-28 22:54:20 +1300

    cleaned some of code bits

--- app/src/main/kotlin/com/planny/activity/NoteActivity.kt
+++ app/src/main/kotlin/com/planny/activity/NoteActivity.kt
@@ -12,15 +12,7 @@ class NoteActivity : ItemActivity() {
 
     override val tag = "Note activity"
     override fun getLayout() = R.layout.activity_note
-    //override fun getActivityTitle() = R.string.app_name
-    /*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val modeToSet = intent.getIntExtra(MODE.EXTRAS_KEY, MODE.VIEW.mode)
-        mode = MODE.getByValue(modeToSet)
-        Log.v(tag, "Mode [ $mode ]")
-    }
-    */
+
 }
 
 
--- app/src/main/kotlin/com/planny/activity/TODOActivity.kt
+++ app/src/main/kotlin/com/planny/activity/TODOActivity.kt
@@ -9,16 +9,6 @@ class TODOActivity : ItemActivity() {
 
     override val tag = "ToDo activity"
     override fun getLayout() = R.layout.activity_todo
-    //override fun getActivityTitle() = R.string.app_name
-
-    /*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val modeToSet = intent.getIntExtra(MODE.EXTRAS_KEY, MODE.VIEW.mode)
-        mode = MODE.getByValue(modeToSet)
-        Log.v(tag, "Mode [ $mode ]")
-    }
-    */
 
 }
 
